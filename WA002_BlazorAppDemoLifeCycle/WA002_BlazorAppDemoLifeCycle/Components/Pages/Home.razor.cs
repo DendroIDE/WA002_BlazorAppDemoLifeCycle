bd285b66151d4d2a2b4f5eff9b93d944dfe02e36
DENNISM_MEGAKONS <dmunoz@megakons.com.ec>
2024-10-31 12:27:08 -0500

    blazor-web-app

---
+++ WA002_BlazorAppDemoLifeCycle/WA002_BlazorAppDemoLifeCycle/Components/Pages/Home.razor.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace WA002_BlazorAppDemoLifeCycle.Components.Pages
+{
+    public partial class Home
+    {
+        public string Valor { get; set; }
+
+        public override async Task SetParametersAsync(ParameterView parameters)
+        {
+            Valor = "Ejecucion de SetParametersAsync";
+            System.Diagnostics.Debug.WriteLine(Valor);
+            await base.SetParametersAsync(parameters);
+        }
+
+        protected override Task OnInitializedAsync()
+        {
+            Valor = "OnInitializedAsync";
+            System.Diagnostics.Debug.WriteLine(Valor);
+            return base.OnInitializedAsync();
+        }
+
+        protected override Task OnParametersSetAsync()
+        {
+            Valor = "OnParametersSetAsync";
+            System.Diagnostics.Debug.WriteLine(Valor);
+            return base.OnParametersSetAsync();
+        }
+
+        protected override Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                Valor = "OnAfterRenderAsync";
+                System.Diagnostics.Debug.WriteLine(Valor);
+                StateHasChanged();
+            }
+            return base.OnAfterRenderAsync(firstRender);
+        }
+    }
+}
